52b4f5625b8438bce53a0f3f6f8c6cb7331049a6
awalch <aymeric.walch@ens-lyon.fr>
2019-01-29 15:22:52 +0100

    Add temporary approximated evaluation

--- basic-calculator/calculator/SLL_interface/interface.cpp
+++ basic-calculator/calculator/SLL_interface/interface.cpp
@@ -9,10 +9,27 @@ std::string evaluate(std::string input)
     if (expr == NULL) {
         return (std::string("Syntax error"));
     } else {
-        giac::context ct; // TODO
+        giac::context ct; // TODO, take context into account
         std::ostringstream result;
         giac::gen e = expr->evaluate();
         result << e << " = " << _evalf(e, &ct) << std::endl;
         return (result.str());
     }
 }
+
+/* This is ugly but it will disappear */
+double approximate(std::string fun_body, std::string antecedent)
+{
+    std::string giac_fun_input;
+    giac_fun_input.append("f(x) := ");
+    giac_fun_input.append(fun_body);
+    std::string giac_eval_input;
+    giac_eval_input.append("f(");
+    giac_eval_input.append(antecedent);
+    giac_eval_input.append(")");
+    giac::context ct;
+    eval(gen(giac_fun_input, &ct), &ct);
+    return (_evalf(giac_eval_input));
+}
+
+
--- basic-calculator/calculator/SLL_interface/interface.hpp
+++ basic-calculator/calculator/SLL_interface/interface.hpp
@@ -4,3 +4,8 @@
 std::string evaluate(std::string input);
 /* Take a SLL string as input and return the string corresponding to the result
 TODO : temporary, will shift to edition tree */
+
+double approximate(std::string fun_body, std::string antecedent);
+/* Take a function expression and an antecedant and return an approximated evaluation
+of it
+TODO : temporary, for graph viewer development */
