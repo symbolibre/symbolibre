6dc29925d6b45b098147eb8cf4d60eaf4b63f9f3
Sébastien Michelland <sebastien.michelland@ens-lyon.fr>
2019-01-31 23:07:54 +0100

    SLL: first thoughts on the interface (not compiled yet)

--- SLL/SLL.hpp
+++ SLL/SLL.hpp
@@ -1,49 +1,58 @@
 #ifndef SLL_HPP
 #define SLL_HPP
 
+#include <giac/config.h>
+#include <giac/giac.h>
 
 namespace SLL
 {
 
-enum Error
-{ NO_ERROR, NAME_EXISTS, NAME_UNKNOWN,};
-
-class Expr
-{
-public:
-    /* empty */
-
-    Error formalEval(Expr target);
-
-    Error approximationEval(Expr, unsigned precision);
+struct Exception: public std::exception {
+    enum Error {
+        NAME_EXISTS,
+        NAME_UNKNOWN,
+        ZERO_DIVISION,
+    };
 };
 
-Expr runCommand(std::string const &command, Context &context);
+/**
+ * SLL::Object - Opaque, structured mathematical objects
+ * Essentially a giac::gen when using Giac, for instance.
+ */
+class Object: private giac::gen
+{
+    /* Numerical approximation */
+    Object approx(unsigned precision);
+};
 
-class Context
+/* Formally evaluate a string into a structured object */
+Object eval(std::string const &formula, Context &context);
+
+/* Execute an SLL command, including variable updates or definitions, that does
+   not return a result */
+void exec(std::string const &command, Context &context);
+
+class Context: private std::map<std::string, Object>
 {
     /* unordered map from variables to SLL_expr */
 public:
     /* ********************** CREATE and SET VARIABLES ********************** */
     /** addVar:
      * @param name: std::string  that is the name of the NEW variable
-     * @param val : Expr that is its value
+     * @param val : Object that is its value (evaluate before adding)
      * The name of the variable has to be new. */
-    Error newVar(std::string name, Expr value);
+    void newVar(std::string name, Object value);
 
     /** setVar:
      * @param name: std::string  that is the name of the NEW variable
-     * @param val : Expr that is its value
+     * @param val : Object that is its value (evaluate before setting)
      * The name of the variable has to exist. */
-    Error setVar(std::string name, Expr value);
+    void setVar(std::string name, Object value);
 
     /* ********************** ACCESS VARIABLES ********************** */
-    Error getVal(std::string name, Expr &res);
-
+    Object getVal(std::string name);
 };
 
-
 }
-
 
 #endif // SLL_HPP
