22be48bd382e3506025595514a4cd22181bd0171
Nicolas Chappe <nicolas.chappe@ens-lyon.fr>
2019-02-02 12:33:20 +0100

    SLL: fix some documentation comments in SLL.hpp

--- SLL/SLL.hpp
+++ SLL/SLL.hpp
@@ -16,23 +16,24 @@ struct Exception: public std::exception {
 };
 
 /**
- * SLL::Object - Opaque, structured mathematical objects
+ * Opaque, structured mathematical object
  * Essentially a giac::gen when using Giac, for instance.
  */
 class Object: private giac::gen
 {
-    /** approx:
+public:
+    /**
      * @param precision: Number of digits of precision
      * @return Same object, with approximated numerical entries */
     Object approx(unsigned precision);
 
-    /** str:
+    /**
      * @return Giac's string representation of the object. */
     std::string str(void);
 };
 
 /**
- * SLL::Context - Evaluation context where the value of variables is stored
+ * Evaluation context where the value of variables is stored
  * This is essentially a map from std::string to SLL::Object.
  */
 class Context: private std::map<std::string, Object>
@@ -40,16 +41,16 @@ class Context: private std::map<std::string, Object>
 public:
     /* ******************* CREATE and SET VARIABLES ******************* */
 
-    /** newVar:
+    /**
      * @param name: Name of the new variable
-     * @param val:  Its value, as an object
+     * @param value:  Its value, as an object
      * The name of the variable has to be new. If you want to assign an
      * expression, evaluate it before with eval(). */
     void newVar(std::string name, Object value);
 
-    /** setVar:
+    /**
      * @param name: Name of an existing variable
-     * @param val:  Its value, as an object
+     * @param value:  Its value, as an object
      * The variable has to exist. If you want to assign an expression,
      * evaluate it before with eval(). */
     void setVar(std::string name, Object value);
@@ -64,17 +65,17 @@ private:
 };
 
 /**
- * eval: Formally evaluate a string into a structured object
+ * Formally evaluate a string into a structured object
  * @param formula: Text formula following the SLL grammar
  * @param context: Evaluation context
  */
 Object eval(std::string const &formula, Context const &context);
 
 /**
- * exec: Execute an SLL command that does not return a result
+ * Execute an SLL command that does not return a result
  * This type of commands includes variables definitions and updates.
  * @param command: Text command following the SLL grammar
- * @param conetxt: Evaluation context, may be modified by the command
+ * @param context: Evaluation context, may be modified by the command
  */
 void exec(std::string const &command, Context &context);
 
