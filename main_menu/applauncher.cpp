46adf4178ed80fedf2c7cf45a630b6cf3fba4e38
Nicolas Chappe <nicolas.chappe@ens-lyon.fr>
2020-07-29 16:25:03 +0200

    main_menu: switch to cmake, clean up directory

--- main_menu/applauncher.cpp
+++ main_menu/applauncher.cpp
@@ -1,4 +1,8 @@
 #include "applauncher.h"
+
+#include <symbolibre/config.hpp>
+
+#include <QDebug>
 #include <QDir>
 #include <QJsonDocument>
 #include <QJsonObject>
@@ -13,14 +17,16 @@ AppItem::AppItem(QObject *parent) : QObject(parent),
 
 AppLauncher::AppLauncher(QObject *parent) : QObject(parent), mAppsModel()
 {
-    QDir appsDir("applications");
+    QDir appsDir(QString(SL_DATA_DIR) + "/applications");
     QStringList apps(appsDir.entryList(QDir::Dirs | QDir::NoDotAndDotDot, QDir::Name));
 
     for(int i = 0; i < apps.size(); i++) {
 
-        QFile file("applications/" + apps[i] + "/application.json");
-        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        QFile file(appsDir.absoluteFilePath(apps[i] + "/application.json"));
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            qWarning() << "unable to read JSON data for application" << apps[i];
             continue;
+        }
 
         auto doc = QJsonDocument::fromJson(file.readAll());
         QJsonObject obj = doc.object();
@@ -30,10 +36,11 @@ AppLauncher::AppLauncher(QObject *parent) : QObject(parent), mAppsModel()
         appData->name = obj["caption"].toString();
         appData->executable = obj["command"].toString().trimmed();
         QString icon = obj["icon"].toString().trimmed();
-        if (icon[0] == '/') {
-            appData->iconPath = "file:" + icon;
+        appData->iconPath = appsDir.absoluteFilePath(apps[i] + "/icon.png");
+        if (QDir::isAbsolutePath(icon)) {
+            appData->iconPath = icon;
         } else {
-            appData->iconPath = "file:applications/" + apps[i] + "/" + icon;
+            appData->iconPath = appsDir.absoluteFilePath(apps[i] + "/" + icon);
         }
         mAppsModel.append(appData);
 
--- main_menu/main.cpp
+++ main_menu/main.cpp
@@ -1,8 +1,11 @@
+#include "applauncher.h"
+
+#include <symbolibre/config.hpp>
+
 #include <QGuiApplication>
 #include <QObject>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include "applauncher.h"
 
 
 int main(int argc, char *argv[])
@@ -14,7 +17,7 @@ int main(int argc, char *argv[])
 
     engine.rootContext()->setContextProperty("launcher", &launcher);
 
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.load(QUrl::fromLocalFile(QString(SL_QML_DIR) + "/menu/main.qml"));
     if (engine.rootObjects().isEmpty())
         return -1;
 
