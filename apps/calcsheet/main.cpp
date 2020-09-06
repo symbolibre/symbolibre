1d60cc7b698ab26458234c0dc3cb69c9266c1eb2
Nicolas Chappe <nicolas.chappe@ens-lyon.fr>
2020-09-06 19:25:05 +0200

    remove applet-specific executables

--- apps/calcsheet/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <symbolibre/symbolibreMain.hpp>
-
-int main(int argc, char *argv[])
-{
-    return symbolibreMain(argc, argv, "calcsheet/main.qml");
-}
--- lib/symbolibre/symbolibreMain.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SL_SYMBOLIBREMAIN_HPP
-#define SL_SYMBOLIBREMAIN_HPP
-
-#include <QString>
-
-int symbolibreMain(int argc, char *argv[], QString componentPath);
-
-#endif // SL_SYMBOLIBREMAIN_HPP
