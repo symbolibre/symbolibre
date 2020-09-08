8f6ed37a53238dcaa3763eedc527d619ebfaad8b
Nicolas Chappe <nicolas.chappe@ens-lyon.fr>
2020-09-09 01:10:27 +0200

    add french translation

---
+++ translations/symbolibre.fr.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>FunApp</name>
+    <message>
+        <source>Confirm</source>
+        <translation>Confirmer</translation>
+    </message>
+    <message>
+        <source>Window</source>
+        <translation>Fenêtre</translation>
+    </message>
+    <message>
+        <source>Graph</source>
+        <translation>Graphe</translation>
+    </message>
+    <message>
+        <source>Function
+input</source>
+        <translation>Entrer
+fonctions</translation>
+    </message>
+</context>
+<context>
+    <name>FunctionMenu</name>
+    <message>
+        <source>Xmin</source>
+        <translation>Xmin</translation>
+    </message>
+    <message>
+        <source>Xmax</source>
+        <translation>Xmax</translation>
+    </message>
+    <message>
+        <source>Ymin</source>
+        <translation>Ymin</translation>
+    </message>
+    <message>
+        <source>Ymax</source>
+        <translation>Ymax</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>The applet to run (defaults to the main menu)</source>
+        <translation>L&apos;applet à lancer (par défaut le menu principal)</translation>
+    </message>
+    <message>
+        <source>List the installed applications and exit</source>
+        <translation>Liste les applications installées et quitte</translation>
+    </message>
+    <message>
+        <source>Show the virtual keyboard</source>
+        <translation>Affiche le clavier virtuel</translation>
+    </message>
+    <message>
+        <source>Unknown application %1</source>
+        <translation>Application %1 inconnue</translation>
+    </message>
+    <message>
+        <source>Application %1 is not an applet</source>
+        <translation>L&apos;application %1 n&apos;est pas un applet</translation>
+    </message>
+    <message>
+        <source>Too many positional arguments</source>
+        <translation>Trop d&apos;arguments positionnels</translation>
+    </message>
+</context>
+<context>
+    <name>SLWindow</name>
+    <message>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Symbolibre</source>
+        <translation>Symbolibre</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <source>Go back to the main menu?</source>
+        <translation>Retourner au menu principal&#xa0;?</translation>
+    </message>
+    <message>
+        <source>Unable to start the QML applet</source>
+        <translation>Impossible de lancer l&apos;applet QML</translation>
+    </message>
+    <message>
+        <source>Unable to start the application</source>
+        <translation>Impossible de lancer l&apos;application</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+</context>
+</TS>
