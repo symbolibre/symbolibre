5883373c6ae6264a3845580ce59c17551abdadb8
Sébastien Michelland <sebastien.michelland@ens-lyon.fr>
2020-10-22 22:59:51 +0200

    translations: update translation-fr

--- translations/symbolibre.fr.ts
+++ translations/symbolibre.fr.ts
@@ -2,6 +2,46 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr">
 <context>
+    <name>CalcSheet</name>
+    <message>
+        <location filename="../apps/calcsheet/CalcSheet.qml" line="96"/>
+        <source>Angles:
+Degrees</source>
+        <translation>Angles:
+Degrés</translation>
+    </message>
+    <message>
+        <location filename="../apps/calcsheet/CalcSheet.qml" line="96"/>
+        <source>Angles:
+Radians</source>
+        <translation>Angles:
+Radians</translation>
+    </message>
+</context>
+<context>
+    <name>Editor</name>
+    <message>
+        <location filename="../apps/ide/Editor.qml" line="166"/>
+        <source>Catalog</source>
+        <translation>Catalogue</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/Editor.qml" line="173"/>
+        <source>Save</source>
+        <translation>Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/Editor.qml" line="177"/>
+        <source>Execute</source>
+        <translation>Exécuter</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/Editor.qml" line="184"/>
+        <source>Undo</source>
+        <translation>Annuler</translation>
+    </message>
+</context>
+<context>
     <name>ExprField</name>
     <message>
         <location filename="../lib/symbolibre/expr/ExprField.qml" line="97"/>
@@ -21,12 +61,43 @@
         <translation>Valider</translation>
     </message>
     <message>
+        <location filename="../apps/funapp/FunApp.qml" line="59"/>
+        <source>Errors occured during plot:</source>
+        <translation>Erreurs durant le tracé :</translation>
+    </message>
+    <message>
+        <location filename="../apps/funapp/FunApp.qml" line="72"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="../apps/funapp/FunApp.qml" line="89"/>
+        <source>Angles:
+Degrees</source>
+        <translation>Angles:
+Degrés</translation>
+    </message>
+    <message>
+        <location filename="../apps/funapp/FunApp.qml" line="89"/>
+        <source>Angles:
+Radians</source>
+        <translation>Angles:
+Radians</translation>
+    </message>
+    <message>
         <source>Window</source>
         <translation>Fenêtre</translation>
     </message>
     <message>
         <source>Graph</source>
         <translation>Graphe</translation>
+    </message>
+    <message>
+        <location filename="../apps/funapp/FunApp.qml" line="160"/>
+        <source>Show
+errors</source>
+        <translation>Afficher
+les erreurs</translation>
     </message>
     <message>
         <source>Function
@@ -71,6 +142,46 @@ fonction</translation>
 </context>
 <context>
     <name>IDE</name>
+    <message>
+        <location filename="../apps/ide/IDE.qml" line="61"/>
+        <source>New file</source>
+        <translation>Nouveau
+fichier</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/IDE.qml" line="78"/>
+        <source>Create a new file</source>
+        <translation>Créer un nouveau fichier</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/IDE.qml" line="83"/>
+        <source>Type</source>
+        <translation>Type</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/IDE.qml" line="115"/>
+        <source>Create</source>
+        <translation>Créer</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/IDE.qml" line="130"/>
+        <source>Open file</source>
+        <translation>Ouvrir un
+fichier</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/IDE.qml" line="145"/>
+        <source>Back to
+files</source>
+        <translation>Retour aux
+fichiers</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/IDE.qml" line="164"/>
+        <location filename="../apps/ide/IDE.qml" line="167"/>
+        <source>Save changes?</source>
+        <translation>Enregistrer les modifications ?</translation>
+    </message>
     <message>
         <location filename="../apps/ide/IDE.qml" line="256"/>
         <source>Create new file</source>
@@ -117,15 +228,18 @@ fonction</translation>
     </message>
 </context>
 <context>
+    <name>SourceEditor</name>
+    <message>
+        <location filename="../apps/ide/SourceEditor.cpp" line="193"/>
+        <source>&lt;Finished&gt;</source>
+        <translation>&lt;Terminé&gt;</translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
         <source>Settings</source>
         <translation>Paramètres</translation>
-    </message>
-    <message>
-        <location filename="../apps/ide/main.qml" line="15"/>
-        <source> - Symbolibre IDE</source>
-        <translation> - IDE Symbolibre</translation>
     </message>
     <message>
         <source>Symbolibre</source>
@@ -146,6 +260,11 @@ fonction</translation>
     <message>
         <source>Unable to start the application</source>
         <translation>Impossible de lancer l&apos;application</translation>
+    </message>
+    <message>
+        <location filename="../apps/ide/main.qml" line="15"/>
+        <source> - Symbolibre IDE</source>
+        <translation> - IDE Symbolibre</translation>
     </message>
 </context>
 </TS>
