35f7daa1964b22b7ffc7db4e35d4bfbd11460910
Sébastien Michelland <sebastien.michelland@ens-lyon.fr>
2020-11-08 11:52:53 +0100

    editiontree: expand on unit tests
    Check evalution results by JSON instead of by string. Add a couple more
    tests on the serializer and evaluator.
    
    Tip: Results from the calcsheet can be obtained with
    % cat <install_path>/history.json | jq -c '.[0].source, .[0].result'

--- tests/editiontree.cpp
+++ tests/editiontree.cpp
@@ -21,8 +21,19 @@ private slots:
     {
         QTest::addColumn<QString>("input");
         QTest::addColumn<bool>("cursor");
-        QTest::newRow("addition") << R"(["1",{"type": "op", "op": "+"},"2"])" << false;
-        QTest::newRow("sqrt") << R"(["",{"type": "sqrt", "content": [["2"]]},""])" << false;
+        QTest::newRow("addition")
+            << R"(["1",{"type": "op", "op": "+"},"2"])" << false;
+        QTest::newRow("sqrt")
+            << R"(["",{"type": "sqrt", "content": [["2"]]},""])" << false;
+        QTest::newRow("frac")
+            << R"(["",{"type": "frac", "content": [["6"],["8"]]},""])" << false;
+        QTest::newRow("frac_plus")
+            << R"(["",{"type": "frac", "content": [["5"],
+                ["7", {"type": "op", "op": "+"}, "5"]]},""])" << false;
+        QTest::newRow("cos")
+            << R"(["cos", {"type": "lparen"}, "0", {"type": "rparen"}, ""])" << false;
+        QTest::newRow("int")
+            << R"(["",{"content":[["x²",{"op":"+","type":"op"},"1"],[""]],"type":"int"},""])" << false;
     }
 
     void evaluation()
@@ -30,14 +41,34 @@ private slots:
         QFETCH(QString, input);
         QFETCH(QString, expected);
         MathContext ctx;
-        QCOMPARE(ctx.evalExpr(input, false), expected.toUtf8());
+        QJsonDocument out = QJsonDocument::fromJson(ctx.evalExpr(input, false).toUtf8());
+        QCOMPARE(out, QJsonDocument::fromJson(expected.toUtf8()));
     }
 
     void evaluation_data()
     {
         QTest::addColumn<QString>("input");
         QTest::addColumn<QString>("expected");
-        QTest::newRow("addition") << R"(["1",{"type": "op", "op": "+"},"2"])" << R"(["3"])";
+        QTest::newRow("addition")
+            << R"(["1",{"type": "op", "op": "+"},"2"])"
+            << R"(["3"])";
+        QTest::newRow("sqrt")
+            << R"(["",{"type": "sqrt", "content": [["2"]]},""])"
+            << R"(["",{"type": "sqrt", "content": [["2"]]},""])";
+        QTest::newRow("frac")
+            << R"(["",{"type": "frac", "content": [["6"],["8"]]},""])"
+            << R"(["",{"type": "frac", "content": [["3"],["4"]]},""])";
+        QTest::newRow("frac_plus")
+            << R"(["",{"type": "frac", "content": [["5"],
+                ["7", {"type": "op", "op": "+"}, "5"]]},""])"
+            << R"(["",{"type": "frac", "content": [["5"],["12"]]},""])";
+        QTest::newRow("cos")
+            << R"(["cos", {"type": "lparen"}, "0", {"type": "rparen"}, ""])"
+            << R"(["1"])";
+        QTest::newRow("int")
+            << R"(["",{"content":[["x²",{"op":"+","type":"op"},"1"],[""]],"type":"int"},""])"
+            << R"(["",{"content":[["x",{"content":[["3"]],"type":"pow"},"",
+                {"op":"+","type":"op"},"3",{"op":"*","type":"op"},"x"],["3"]],"type":"frac"},""])";
     }
 };
 
